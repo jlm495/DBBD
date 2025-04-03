23a4a82facc3f7a46d7b9b9912d53da627d5a168
Jenna Mandel <jema7620@colorado.edu>
2025-04-02 19:38:39 -0600

    revamp site

--- js/main.js
+++ js/main.js
@@ -6,14 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.querySelector('.navbar');
     const toggleButton = document.getElementById('bgToggle');
     const menuButton = document.createElement('button');
+    const pageBG = document.querySelector('body');
     
     // Background cycling functionality
     const backgrounds = [
-        'url("./img/DBBD_blue_checkers.jpg")',
-        'url("./img/DBBD_red_checkers.jpg")',
-        'url("./img/DBBD_orange_checkers.jpg")',
-        'url("./img/DBBD_green_checkers.jpg")',
-        'url("./img/DBBD_yellow_checkers.jpg")'
+        'url("DBBD_black_checker.jpg")',
+        'url("DBBD_blue_checkers.jpg")',
+        'url("DBBD_red_checkers.jpg")',
+        'url("DBBD_orange_checkers.jpg")',
+        'url("DBBD_green_checkers.jpg")',
+        'url("DBBD_yellow_checkers.jpg")'
     ];
     
     // Get saved background index or default to 0
@@ -21,10 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Apply saved background on page load
     navbar.style.backgroundImage = backgrounds[currentBg];
+    pageBG.style.backgroundImage = backgrounds[currentBg];
 
     toggleButton.addEventListener('click', function() {
         currentBg = (currentBg + 1) % backgrounds.length;
         navbar.style.backgroundImage = backgrounds[currentBg];
+        pageBG.style.backgroundImage = backgrounds[currentBg];
         // Save the choice
         localStorage.setItem('navbarBackground', currentBg);
     });
